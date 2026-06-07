3cba39267fe2a64610c41f829011398ca643640f
Clarissa Richard <107963214+clarissarichard@users.noreply.github.com>
2026-06-06 21:31:40 -0600

    Add more types

--- constants.js
+++ constants.js
@@ -25,5 +25,70 @@ export const TYPE_CHART = {
         strongAgainst: ["grass", "fighting", "bug"],
         weakAgainst: ["electric", "rock", "steel"],
         noEffectAgainst: []
-        }
-    };
+        },
+    electric: {
+        strongAgainst: ["water", "flying"],
+        weakAgainst: ["electric", "grass", "dragon"],
+        noEffectAgainst: ["ground"]
+    },
+    ice: {
+        strongAgainst: ["grass", "ground", "flying", "dragon"],
+        weakAgainst: ["fire", "water", "ice", "steel"],
+        noEffectAgainst: []
+    },
+    fighting: {
+        strongAgainst: ["normal", "ice", "rock", "dark", "steel"],
+        weakAgainst: ["poison", "flying", "psychic", "bug", "fairy"],
+        noEffectAgainst: ["ghost"]
+    },
+    ground: {
+        strongAgainst: ["fire", "electric", "poison", "rock", "steel"],
+        weakAgainst: ["grass", "bug"],
+        noEffectAgainst: ["flying"]
+    },
+    rock: {
+        strongAgainst: ["fire", "ice", "flying", "bug"],
+        weakAgainst: ["fighting", "ground", "steel"],
+        noEffectAgainst: []
+    },
+    bug: {
+        strongAgainst: ["grass", "psychic", "dark"],
+        weakAgainst: ["fire", "fighting", "poison", "flying", "ghost", "steel", "fairy"],
+        noEffectAgainst: []
+    },
+    ghost: {
+        strongAgainst: ["psychic", "ghost"],
+        weakAgainst: ["dark"],
+        noEffectAgainst: ["normal"]
+    },
+    steel: {
+        strongAgainst: ["ice", "rock", "fairy"],
+        weakAgainst: ["fire", "water", "electric", "steel"],
+        noEffectAgainst: []
+    },
+    fairy: {
+        strongAgainst: ["fighting", "dragon", "dark"],
+        weakAgainst: ["fire", "poison", "steel"],
+        noEffectAgainst: []
+    },
+    normal: {
+        strongAgainst: [],
+        weakAgainst: ["rock", "steel"],
+        noEffectAgainst: ["ghost"]
+    },
+    psychic: {
+        strongAgainst: ["fighting", "poison"],
+        weakAgainst: ["psychic", "steel"],
+        noEffectAgainst: ["dark"]
+    },
+    dragon: {
+        strongAgainst: ["dragon"],
+        weakAgainst: ["steel"],
+        noEffectAgainst: ["fairy"]
+    },
+    dark: {
+        strongAgainst: ["ghost", "psychic"],
+        weakAgainst: ["fighting", "dark", "fairy"],
+        noEffectAgainst: []
+    }
+};
