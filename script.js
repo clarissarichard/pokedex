ee6d5dafd3c8705a513cef050346528d96726aa8
Clarissa Richard <107963214+clarissarichard@users.noreply.github.com>
2026-06-16 15:46:37 -0600

    Add search bar

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 import { POKEAPI_BASE_URL, POKEMON_FETCH_LIMIT } from "./constants.js";
 import { getPokemonDetails, renderPokemonCards } from "./pokemon.js";
 
+let allPokemon = [];
+
 async function fetchAllPokemon() {
     const response = await fetch(`${POKEAPI_BASE_URL}/pokemon?limit=${POKEMON_FETCH_LIMIT}`);
     const data = await response.json();
@@ -11,13 +13,26 @@ async function fetchAllPokemon() {
             return getPokemonDetails(details);
         })
     );
+    allPokemon = pokemonDetails;
     displayPokemon(pokemonDetails);
+    searchPokemon();
 }
 
 function displayPokemon(pokemonList) {
     const grid = document.querySelector(".pokemon-grid");
 
     grid.innerHTML = renderPokemonCards(pokemonList);
+}
+
+function searchPokemon() {
+    const searchInput = document.getElementById("searchBar");
+    searchInput.addEventListener("input", () => {
+        const query = searchInput.value.toLowerCase().trim();
+        const filteredPokemon = allPokemon.filter((pokemon) =>
+            pokemon.name.toLowerCase().includes(query)
+        );
+        displayPokemon(filteredPokemon);
+    });
 }
 
 fetchAllPokemon();
